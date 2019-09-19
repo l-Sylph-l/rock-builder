275d2a8bf186aaf1cf858167718df8f1561f5ab9
Kim <kim.hunkeler@gmx.ch>
2019-09-19 10:40:21 +0200

    Erste UI Anpassungen / Tests
    RockBuilder ist nun unter Tools zu finden und kann auch geöffnet werden.

--- Assets/Rock Builder/Assets/Scripts/UI/UiSample.cs
+++ Assets/Rock Builder/Assets/Scripts/UI/UiSample.cs
@@ -1,18 +1,45 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEditor;
 
-public class UiSample : MonoBehaviour
+public class RockBuilderWindow : EditorWindow
 {
-    // Start is called before the first frame update
-    void Start()
+    string firstParam = "Object Name";
+    Color color;
+
+    [MenuItem("Tools/RockBuilder")]
+    public static void ShowWindow ()
     {
-        
+        GetWindow<RockBuilderWindow>("Rock Builder");
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnGUI ()
     {
-        
+        // Code für das UI des RockBuilder Fensters
+        GUILayout.Label("Rock Builder 2019", EditorStyles.boldLabel);
+        firstParam = EditorGUILayout.TextField("Object Name", firstParam);
+
+        // Color Test
+        color = EditorGUILayout.ColorField("Color", color);
+
+        if (GUILayout.Button("Colorize Objects"))
+        {
+            Debug.Log("Stones Button was pressed"); // Gibt eine Logmeldung aus
+
+            Colorize();
+        }
+    }
+
+    // Testmethode um etwas einzufärben
+    void Colorize()
+    {
+        foreach (GameObject obj in Selection.gameObjects)
+        {
+            Renderer renderer = obj.GetComponent<Renderer>();
+
+            if (renderer != null)
+            {
+                renderer.sharedMaterial.color = color;
+            }
+        }
     }
 }
