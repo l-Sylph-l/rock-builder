733b8ec525ce5b891b1dd4aa0fa680779c312943
Kim <kim.hunkeler@gmx.ch>
2019-10-09 11:13:11 +0200

    Lot of UI stuff done
    Started with the UI, already lot of things done! Next step is to have pictures in the buttons for the shapes, materials...

--- Assets/Rock Builder/Assets/Scripts/UI/UiSample.cs
+++ Assets/Rock Builder/Assets/Scripts/UI/UiSample.cs
@@ -1,45 +1,197 @@
 ﻿using UnityEngine;
 using UnityEditor;
+using UnityEngine.UIElements;
+using UnityEngine.EventSystems;
 
 public class RockBuilderWindow : EditorWindow
 {
-    string firstParam = "Object Name";
-    Color color;
+    private GUIStyle guiStyle = new GUIStyle();
+    private GUIStyle guiColor = new GUIStyle();
+    int toolbarInt = 0;
+    string[] toolbarStrings = { "Hello", "Stones", "Crystals" };
+
+    // Alle Bilder für die Buttons (funktioniert leider nocht nicht...)
+    private Texture shape_crystal;
+    private Texture shape_gem;
+    private Texture shape_diamond;
+
+    // Parameter für die Steine
+    string firstParameterStones = "Stone_01";
+    bool secondParameterStones = false;
+    float thirdParamaterStones = 10.0f;
+    int fourthParamaterStones = 0;
+    string fifthParameterStones = "MAT_Granite";
+
+    // Parameter für die Kristalle/Edelsteine
+    string firstParameterCrystals = "Crystal";
+    string secondParameterCrystals = "MAT_Gem_01";
 
     [MenuItem("Tools/RockBuilder")]
-    public static void ShowWindow ()
+
+    public static void ShowWindow()
     {
         GetWindow<RockBuilderWindow>("Rock Builder");
     }
 
-    void OnGUI ()
+    void OnGUI()
     {
-        // Code für das UI des RockBuilder Fensters
-        GUILayout.Label("Rock Builder 2019", EditorStyles.boldLabel);
-        firstParam = EditorGUILayout.TextField("Object Name", firstParam);
+        // Code für das UI des RockBuilder Fensters ALLE Tabs
+        toolbarInt = GUILayout.Toolbar(toolbarInt, toolbarStrings);
 
-        // Color Test
-        color = EditorGUILayout.ColorField("Color", color);
-
-        if (GUILayout.Button("Colorize Objects"))
+        // Anzeige für den Startteil => Auswahl ob Steine oder Kristalle + Infos
+        if (toolbarInt == 0)
         {
-            Debug.Log("Stones Button was pressed"); // Gibt eine Logmeldung aus
+            GUILayout.Space(5);
 
-            Colorize();
-        }
-    }
+            guiStyle.fontSize = 20;
+            GUILayout.Label("Rock Builder", guiStyle);
 
-    // Testmethode um etwas einzufärben
-    void Colorize()
-    {
-        foreach (GameObject obj in Selection.gameObjects)
-        {
-            Renderer renderer = obj.GetComponent<Renderer>();
+            GUILayout.Space(15);
 
-            if (renderer != null)
+            EditorGUILayout.HelpBox("Rock Builder is a simple tool to create stones and crystals with different shaders. It's made by two students from the SAE Zurich as their bachelor thesis. Have fun!", MessageType.Info);
+
+            GUILayout.Space(20);
+
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button("Stones"))
             {
-                renderer.sharedMaterial.color = color;
+                Debug.Log("Stone Button was pressed"); // Gibt eine Logmeldung aus
+                toolbarInt = 1;
             }
+            if (GUILayout.Button("Crystals"))
+            {
+                Debug.Log("Crystal Button was pressed"); // Gibt eine Logmeldung aus
+                toolbarInt = 2;
+            }
+            GUILayout.EndHorizontal();
+        }
+
+        // Anzeige für den "Stones"-Teil
+        if (toolbarInt == 1)
+        {
+            GUILayout.Space(5);
+
+            guiStyle.fontSize = 20;
+            guiColor.normal.textColor = Color.black;
+            GUILayout.Label("Rock Builder - Stones", guiStyle);
+
+            GUILayout.Space(20);
+
+            // Erster Stones-Parameter => Der Name des Objekts(Steins), welches anschliessend generiert werden soll           
+            firstParameterStones = EditorGUILayout.TextField("Object Name", firstParameterStones);
+
+            // Zweiter Paramter => Die Checkbox, ob der User eine eigene Form extruden oder lieber die Defaultform (rund) haben möchte           
+            secondParameterStones = EditorGUILayout.Toggle("Make your own shape", secondParameterStones);
+            EditorGUILayout.HelpBox("By clicking 'Make your own shape', a cube will appear in the editor window. Extrude his faces to make the shape you want. If you don't click the checkbox, the generated stone will be round by default.", MessageType.Info);
+
+            // Dritter Parameter => Slidebar für den gewünschten Polycount zwischen 10 und 10'000  
+            thirdParamaterStones = EditorGUILayout.Slider("Polycount", thirdParamaterStones, 10, 10000);
+
+            // Vierter Parameter => Slidebar für die Anzahl der LODs  
+            fourthParamaterStones = EditorGUILayout.IntSlider("LODs", fourthParamaterStones, 0, 3);
+
+            GUILayout.Space(15);
+
+            // Auswahl der Stein-Materialien      
+            GUILayout.Label("Choose Material");
+
+            GUILayout.Space(5);
+
+            EditorGUI.BeginDisabledGroup(true);
+            EditorGUILayout.TextField(fifthParameterStones);
+            EditorGUI.EndDisabledGroup();
+
+            GUILayout.Space(10);
+
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button("Granite", GUILayout.Height(60), GUILayout.Width(55)))
+            {
+                Debug.Log("MAT_Granite Button was pressed"); // Gibt eine Logmeldung aus
+                fifthParameterStones = "MAT_Granite";
+            }
+            if (GUILayout.Button("Sandstone", GUILayout.Height(60), GUILayout.Width(55)))
+            {
+                Debug.Log("MAT_Sandstone Button was pressed"); // Gibt eine Logmeldung aus
+                fifthParameterStones = "MAT_Sandstone";
+            }
+            if (GUILayout.Button("Limestone", GUILayout.Height(60), GUILayout.Width(55)))
+            {
+                Debug.Log("MAT_Limestone Button was pressed"); // Gibt eine Logmeldung aus
+                fifthParameterStones = "MAT_Limestone";
+            }
+            GUILayout.EndHorizontal();
+        }
+
+        // Anzeige für den "Crystals"-Teil
+        if (toolbarInt == 2)
+        {
+            GUILayout.Space(5);
+
+            guiStyle.fontSize = 20;
+            guiColor.normal.textColor = Color.black;
+            GUILayout.Label("Rock Builder - Crystals", guiStyle);
+
+            GUILayout.Space(20);
+
+            // Auswahl der Art des Kristalls/Edelstein     
+            GUILayout.Label("Choose Shape");
+
+            GUILayout.Space(5);
+
+            EditorGUI.BeginDisabledGroup(true);
+            EditorGUILayout.TextField(firstParameterCrystals);
+            EditorGUI.EndDisabledGroup();
+
+            GUILayout.Space(10);
+
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button("Crystal", GUILayout.Height(60), GUILayout.Width(55)))
+            {
+                Debug.Log("Crystal Button was pressed"); // Gibt eine Logmeldung aus
+                firstParameterCrystals = "Crystal";
+            }
+            if (GUILayout.Button("Gem", GUILayout.Height(60), GUILayout.Width(55)))
+            {
+                Debug.Log("Gem Button was pressed"); // Gibt eine Logmeldung aus
+                firstParameterCrystals = "Gem";
+            }
+            if (GUILayout.Button("Diamond", GUILayout.Height(60), GUILayout.Width(55)))
+            {
+                Debug.Log("Diamond Button was pressed"); // Gibt eine Logmeldung aus
+                firstParameterCrystals = "Diamond";
+            }
+            GUILayout.EndHorizontal();
+
+            GUILayout.Space(15);
+
+            // Auswahl der Kristall-Materialien      
+            GUILayout.Label("Choose Material");
+
+            GUILayout.Space(5);
+
+            EditorGUI.BeginDisabledGroup(true);
+            EditorGUILayout.TextField(secondParameterCrystals);
+            EditorGUI.EndDisabledGroup();
+
+            GUILayout.Space(10);
+
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button("Gem 01", GUILayout.Height(60), GUILayout.Width(55)))
+            {
+                Debug.Log("MAT_Gem_01 Button was pressed"); // Gibt eine Logmeldung aus
+                secondParameterCrystals = "MAT_Gem_01";
+            }
+            if (GUILayout.Button("Gem 02", GUILayout.Height(60), GUILayout.Width(55)))
+            {
+                Debug.Log("MAT_Gem_02 Button was pressed"); // Gibt eine Logmeldung aus
+                secondParameterCrystals = "MAT_Gem_02";
+            }
+            if (GUILayout.Button("Gem 03", GUILayout.Height(60), GUILayout.Width(55)))
+            {
+                Debug.Log("MAT_Gem_03 Button was pressed"); // Gibt eine Logmeldung aus
+                secondParameterCrystals = "MAT_Gem_03";
+            }
+            GUILayout.EndHorizontal();
         }
     }
 }
