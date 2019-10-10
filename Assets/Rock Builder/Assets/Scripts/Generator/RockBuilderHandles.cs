e12fb963dd65cfabb71e0561e64440608922f706
Stefano <c.stefano6@gmail.com>
2019-10-10 09:32:53 +0200

    Added first generator scripts

---
+++ Assets/Rock Builder/Assets/Scripts/Generator/RockBuilderHandles.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TestCubeGeneration))]
+public class RockBuilderHandles : Editor
+{
+
+    TestCubeGeneration testCube;
+
+    void OnEnable()
+    {
+        testCube = (TestCubeGeneration)target;
+    }
+
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+    }
+
+    void OnSceneGUI()
+    {
+        Handles.Label(testCube.transform.position + new Vector3(0f, 2f, 0f), testCube.name);
+    }
+}
