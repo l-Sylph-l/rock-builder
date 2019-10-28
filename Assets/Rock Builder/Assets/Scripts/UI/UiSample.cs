a91e41c448b0ecf6049ec65a912b3c3db22193f6
Nefaru90 <kim.hunkeler@gmx.ch>
2019-10-28 10:54:10 +0100

    UI changes
    Es wurden diverse Änderungen nach dem Gespräch mit Stefan Alexe vorgenommen. Das UI besitzt nun einen Help-Tab, dafür keinen Hello-Tab mehr. Begriffe wurden geändert und neue Funktionen eingebaut. Zusätzlich gibt es 3 neue Icons und ein Test PDF-File.

--- Assets/Rock Builder/Assets/Scripts/UI/UiSample.cs
+++ Assets/Rock Builder/Assets/Scripts/UI/UiSample.cs
@@ -10,24 +10,25 @@ public class RockBuilderWindow : EditorWindow
     private GUIStyle guiStyle = new GUIStyle();
     private GUIStyle guiColor = new GUIStyle();
     int toolbarInt = 0;
-    string[] toolbarStrings = { "Hello", "Stones", "Crystals" };
+    string[] toolbarStrings = { "Rocks", "Gemstones", "Help" };
 
     // Parameter für die Steine
-    string firstParameterStones = "Stone_01"; // Objektname
-    bool secondParameterStones = false; // Eigene Shape erstellen
-    int thirdParamaterStones = 10; // Polycount
-    int fourthParamaterStones = 0; // LODs
-    private Material stoneMaterial; // Material
+    string firstParameterRocks = "Stone_01"; // Objektname
+    bool secondParameterRocks = false; // Eigene Shape erstellen
+    int thirdParamaterRocks = 10; // Polycount
+    int fourthParamaterRocks = 0; // LODs
+    private Material rockMaterial; // Material
 
     // Parameter für die Kristalle/Edelsteine
-    string firstParameterCrystals = "Crystal_01"; // Objektname
-    string secondParameterCrystals = "Crystal"; // Shape
-    int thirdParamaterCrystals = 3; // Vertices
-    float fourthParamaterCrystals = 1.0f; // Radius
-    float fifthParamaterCrystals = 1.0f; // Height
-    float sixthParamaterCrystals = 1.0f; // Peak Height
-    bool seventhParameterCrystals = false; // Smooth
-    private Material crystalMaterial; // Material
+    string firstParameterGemstones = "Crystal_01"; // Objektname
+    string secondParameterGemstones = "Crystal"; // Shape
+    int thirdParamaterGemstones = 3; // Vertices
+    float fourthParamaterGemstones = 1.0f; // Radius
+    float fifthParamaterGemstones = 1.0f; // Height
+    float sixthParamaterGemstones = 1.0f; // Peak Height
+    bool seventhParameterGemstones = false; // Smooth
+    private Material gemstoneMaterial; // Material
+    float specialParameterDiamond = 0.5f; // Top Radius Diamant
 
     [MenuItem("Tools/RockBuilder")]
 
@@ -41,57 +42,29 @@ public class RockBuilderWindow : EditorWindow
         // Code für das UI des RockBuilder Fensters ALLE Tabs
         toolbarInt = GUILayout.Toolbar(toolbarInt, toolbarStrings);
 
-        // Anzeige für den Startteil => Auswahl ob Steine oder Kristalle + Infos
+        // Anzeige für den "Rocks"-Teil
         if (toolbarInt == 0)
         {
             GUILayout.Space(5);
 
             guiStyle.fontSize = 20;
-            GUILayout.Label("Rock Builder", guiStyle);
-
-            GUILayout.Space(15);
-
-            EditorGUILayout.HelpBox("Rock Builder is a simple tool to create stones and crystals with different shaders. It's made by two students from the SAE Zurich as their bachelor thesis. Have fun!", MessageType.Info);
-
-            GUILayout.Space(20);
-
-            GUILayout.BeginHorizontal();
-            if (GUILayout.Button("Stones"))
-            {
-                Debug.Log("Stone Button was pressed"); // Gibt eine Logmeldung aus
-                toolbarInt = 1;
-            }
-            if (GUILayout.Button("Crystals"))
-            {
-                Debug.Log("Crystal Button was pressed"); // Gibt eine Logmeldung aus
-                toolbarInt = 2;
-            }
-            GUILayout.EndHorizontal();
-        }
-
-        // Anzeige für den "Stones"-Teil
-        if (toolbarInt == 1)
-        {
-            GUILayout.Space(5);
-
-            guiStyle.fontSize = 20;
             guiColor.normal.textColor = Color.black;
-            GUILayout.Label("Rock Builder - Stones", guiStyle);
+            GUILayout.Label("Rock Builder - Rocks", guiStyle);
 
             GUILayout.Space(20);
 
-            // Erster Stones-Parameter => Der Name des Objekts(Steins), welches anschliessend generiert werden soll           
-            firstParameterStones = EditorGUILayout.TextField("Object Name", firstParameterStones);
+            // Erster Rocks-Parameter => Der Name des Objekts(Steins), welches anschliessend generiert werden soll           
+            firstParameterRocks = EditorGUILayout.TextField("Object Name", firstParameterRocks);
 
-            // Zweiter Stones-Paramter => Die Checkbox, ob der User eine eigene Form extruden oder lieber die Defaultform (rund) haben möchte           
-            secondParameterStones = EditorGUILayout.Toggle("Make your own shape", secondParameterStones);
-            EditorGUILayout.HelpBox("By clicking 'Make your own shape', a cube will appear in the editor window. Extrude his faces to make the shape you want. If you don't click the checkbox, the generated stone will be round by default.", MessageType.Info);
+            // Zweiter Rocks-Paramter => Die Checkbox, ob der User eine eigene Form extruden oder lieber die Defaultform (rund) haben möchte           
+            secondParameterRocks = EditorGUILayout.Toggle("Make your own shape", secondParameterRocks);
+            EditorGUILayout.HelpBox("By clicking 'Make your own shape', a cube will appear in the editor window. Extrude his faces to make the shape you want. If you don't click the checkbox, the generated rock will be round by default.", MessageType.Info);
 
-            // Dritter Stones-Parameter => Slidebar für den gewünschten Polycount zwischen 10 und 10'000  
-            thirdParamaterStones = EditorGUILayout.IntSlider("Polycount", thirdParamaterStones, 10, 10000);
+            // Dritter Rocks-Parameter => Slidebar für den gewünschten Polycount zwischen 10 und 10'000  
+            thirdParamaterRocks = EditorGUILayout.IntSlider("Polycount", thirdParamaterRocks, 10, 10000);
 
-            // Vierter Stones-Parameter => Slidebar für die Anzahl der LODs  
-            fourthParamaterStones = EditorGUILayout.IntSlider("LODs", fourthParamaterStones, 0, 3);
+            // Vierter Rocks-Parameter => Slidebar für die Anzahl der LODs  
+            fourthParamaterRocks = EditorGUILayout.IntSlider("LODs", fourthParamaterRocks, 0, 3);
 
             GUILayout.Space(15);
 
@@ -103,7 +76,7 @@ public class RockBuilderWindow : EditorWindow
             // Diese Prüfung entscheidet, welche Shader angezeigt werden => Lightweight/Universal oder HD-Renderpipeline
             if (RenderPipelineManager.currentPipeline != null && RenderPipelineManager.currentPipeline.ToString().Contains("HD"))
             {
-                stoneMaterial = (Material)EditorGUILayout.ObjectField(stoneMaterial, typeof(Material), true);
+                rockMaterial = (Material)EditorGUILayout.ObjectField(rockMaterial, typeof(Material), true);
             }
             // else steht für die Lightweight/Universal Pipeline oder gar keine
             else
@@ -116,23 +89,23 @@ public class RockBuilderWindow : EditorWindow
             // Button für das Generieren des Steins
             if (GUILayout.Button("Let's rock!", GUILayout.Height(25)))
             {
-                Debug.Log("Stone-Generate Button was pressed"); // Gibt eine Logmeldung aus
+                Debug.Log("Rocks-Generate Button was pressed"); // Gibt eine Logmeldung aus
             }
         }
 
-        // Anzeige für den "Crystals"-Teil
-        if (toolbarInt == 2)
+        // Anzeige für den "Gemstones"-Teil
+        if (toolbarInt == 1)
         {
             GUILayout.Space(5);
 
             guiStyle.fontSize = 20;
             guiColor.normal.textColor = Color.black;
-            GUILayout.Label("Rock Builder - Crystals", guiStyle);
+            GUILayout.Label("Rock Builder - Gemstones", guiStyle);
 
             GUILayout.Space(20);
 
-            // Erster Crystal-Parameter => Der Name des Objekts, welches anschliessend generiert werden soll           
-            firstParameterCrystals = EditorGUILayout.TextField("Object Name", firstParameterCrystals);
+            // Erster Gemstones-Parameter => Der Name des Objekts, welches anschliessend generiert werden soll           
+            firstParameterGemstones = EditorGUILayout.TextField("Object Name", firstParameterGemstones);
 
             GUILayout.Space(15);
 
@@ -142,73 +115,85 @@ public class RockBuilderWindow : EditorWindow
             GUILayout.Space(5);
 
             EditorGUI.BeginDisabledGroup(true);
-            EditorGUILayout.TextField(secondParameterCrystals);
+            EditorGUILayout.TextField(secondParameterGemstones);
             EditorGUI.EndDisabledGroup();
 
             GUILayout.Space(10);
 
             GUILayout.BeginHorizontal();
-            GUILayout.Box(LoadPNG("Assets/Rock Builder/Assets/Images/gizmo_raycast_crosshaair.png"), new GUILayoutOption[] { GUILayout.Width(50), GUILayout.Height(50) });
+            GUILayout.Box(LoadPNG("Assets/Rock Builder/Assets/Images/Crystal_icon.png"), new GUILayoutOption[] { GUILayout.Width(30), GUILayout.Height(30) });
             if (GUILayout.Button(" Crystal ", GUILayout.Height(60)))
             {
                 Debug.Log("Crystal Button was pressed"); // Gibt eine Logmeldung aus
-                secondParameterCrystals = "Crystal";
+                secondParameterGemstones = "Crystal";
             }
-
+            GUILayout.Box(LoadPNG("Assets/Rock Builder/Assets/Images/Gem_icon.png"), new GUILayoutOption[] { GUILayout.Width(30), GUILayout.Height(30) });
             if (GUILayout.Button("   Gem   ", GUILayout.Height(60)))
             {
                 Debug.Log("Gem Button was pressed"); // Gibt eine Logmeldung aus
-                secondParameterCrystals = "Gem";
+                secondParameterGemstones = "Gem";
             }
+            GUILayout.Box(LoadPNG("Assets/Rock Builder/Assets/Images/Diamond_icon.png"), new GUILayoutOption[] { GUILayout.Width(30), GUILayout.Height(30) });
             if (GUILayout.Button("Diamond", GUILayout.Height(60)))
             {
                 Debug.Log("Diamond Button was pressed"); // Gibt eine Logmeldung aus
-                secondParameterCrystals = "Diamond";
+                secondParameterGemstones = "Diamond";
             }
             GUILayout.EndHorizontal();
 
             GUILayout.Space(15);
 
-            // Dritter Crystal-Parameter => Slidebar für die Anzahl Vertices  
-            thirdParamaterCrystals = EditorGUILayout.IntSlider("Vertices", thirdParamaterCrystals, 3, 200);
+            // Dritter Gemstones-Parameter => Slidebar für die Anzahl Vertices  
+            thirdParamaterGemstones = EditorGUILayout.IntSlider("Vertices", thirdParamaterGemstones, 3, 200);
 
-            // Beschränkt die Usereingaben für den Radius => 0.1 - 1000
-            if (fourthParamaterCrystals < 0.1 || fourthParamaterCrystals > 1000)
+            // Beschränkt die Usereingaben für den Radius => 0.01 - 1000
+            if (fourthParamaterGemstones < 0.01 || fourthParamaterGemstones > 1000)
             {
-                fourthParamaterCrystals = 1.0f;
+                fourthParamaterGemstones = 1.0f;
             }
-            // Vierter Crystal-Parameter für den Radius der Objekte
-            fourthParamaterCrystals = EditorGUILayout.FloatField("Radius", fourthParamaterCrystals);
+            // Vierter Gemstones-Parameter für den Radius der Objekte
+            fourthParamaterGemstones = EditorGUILayout.FloatField("Radius", fourthParamaterGemstones);
 
-            // Beschränkt die Usereingaben für die Höhe => 0.1 - 1000
-            if (fifthParamaterCrystals < 0.1 || fifthParamaterCrystals > 1000)
+            // Diese Eingabe wird nur bei Diamanten benötigt, deshalb wird hier geprüft
+            if (secondParameterGemstones == "Diamond")
             {
-                fifthParamaterCrystals = 1.0f;
+                // Beschränkt die Usereingaben für den Top Radius der Diamanten => 0.01 - 1000
+                if (specialParameterDiamond < 0.01 || specialParameterDiamond > 1000)
+                {
+                    specialParameterDiamond = 1.0f;
+                }
+                specialParameterDiamond = EditorGUILayout.FloatField("Top Radius", specialParameterDiamond);
             }
-            // Fünfter Crystal-Parameter für die Höhe der Objekte
-            fifthParamaterCrystals = EditorGUILayout.FloatField("Height", fifthParamaterCrystals);
 
-            // Beschränkt die Usereingaben für die Höhe => 0.1 - 100
-            if (sixthParamaterCrystals < 0.1 || sixthParamaterCrystals > 100)
+            // Beschränkt die Usereingaben für die Höhe => 0.01 - 1000
+            if (fifthParamaterGemstones < 0.01 || fifthParamaterGemstones > 1000)
             {
-                sixthParamaterCrystals = 1.0f;
+                fifthParamaterGemstones = 1.0f;
             }
-            // Sechster Crystal-Parameter für die Höhe der Spitze
-            sixthParamaterCrystals = EditorGUILayout.FloatField("Peak Height", sixthParamaterCrystals);
+            // Fünfter Gemstones-Parameter für die Höhe der Objekte
+            fifthParamaterGemstones = EditorGUILayout.FloatField("Height", fifthParamaterGemstones);
 
-            // Siebter Crystal-Paramter => Die Checkbox, um ein Objekt zu smoothen         
-            seventhParameterCrystals = EditorGUILayout.Toggle("Smooth", seventhParameterCrystals);
+            // Beschränkt die Usereingaben für die Höhe => 0.01 - 100
+            if (sixthParamaterGemstones < 0.01 || sixthParamaterGemstones > 100)
+            {
+                sixthParamaterGemstones = 1.0f;
+            }
+            // Sechster Gemstones-Parameter für die Höhe der Spitze
+            sixthParamaterGemstones = EditorGUILayout.FloatField("Peak Height", sixthParamaterGemstones);
+
+            // Siebter Gemstones-Paramter => Die Checkbox, um ein Objekt zu smoothen         
+            seventhParameterGemstones = EditorGUILayout.Toggle("Smooth", seventhParameterGemstones);
 
             GUILayout.Space(15);
 
-            // Auswahl der Kristall-Materialien      
+            // Auswahl der Gemstones-Materialien      
             GUILayout.Label("Choose Material");
 
             GUILayout.Space(5);
 
             // Diese Prüfung entscheidet, welche Shader angezeigt werden => Lightweight/Universal oder HD-Renderpipeline (wird momentan nicht mehr benötigt)
             //if (RenderPipelineManager.currentPipeline != null && RenderPipelineManager.currentPipeline.ToString().Contains("HD"))
-            crystalMaterial = (Material)EditorGUILayout.ObjectField(crystalMaterial, typeof(Material), true);
+            gemstoneMaterial = (Material)EditorGUILayout.ObjectField(gemstoneMaterial, typeof(Material), true);
 
             GUILayout.Space(15);
 
@@ -222,10 +207,10 @@ public class RockBuilderWindow : EditorWindow
             // update all values if diamondgenerator isn't null
             if (diamondGenerator)
             {
-                diamondGenerator.previewRadius = fourthParamaterCrystals;
-                diamondGenerator.previewHeight = fifthParamaterCrystals;
-                diamondGenerator.previewHeightPeak = sixthParamaterCrystals;
-                diamondGenerator.previewEdges = thirdParamaterCrystals;
+                diamondGenerator.previewRadius = fourthParamaterGemstones;
+                diamondGenerator.previewHeight = fifthParamaterGemstones;
+                diamondGenerator.previewHeightPeak = sixthParamaterGemstones;
+                diamondGenerator.previewEdges = thirdParamaterGemstones;
                 diamondGenerator.showPreview = true;
             }
 
@@ -238,21 +223,53 @@ public class RockBuilderWindow : EditorWindow
                 {
                     //Undo.RecordObjects(new Object[] { diamondGenerator, diamondGenerator.transform.GetComponent<MeshFilter>().sharedMesh }, "Diamond modified");
                     Undo.RegisterFullObjectHierarchyUndo(diamondGenerator, "Diamond modified");
-                    diamondGenerator.CreateMesh(fourthParamaterCrystals, fifthParamaterCrystals, sixthParamaterCrystals, thirdParamaterCrystals, seventhParameterCrystals, crystalMaterial);
+                    diamondGenerator.CreateMesh(fourthParamaterGemstones, fifthParamaterGemstones, sixthParamaterGemstones, thirdParamaterGemstones, seventhParameterGemstones, gemstoneMaterial);
                 }
                 else
                 {
                     Transform cameraTransform = SceneView.lastActiveSceneView.camera.transform;
                     diamondGenerator = new GameObject().AddComponent(typeof(DiamondGenerator)) as DiamondGenerator;
                     Undo.RegisterCreatedObjectUndo(diamondGenerator, "Created diamond");
-                    diamondGenerator.transform.position = (cameraTransform.forward * (fourthParamaterCrystals * 3f + fifthParamaterCrystals * 2f)) + cameraTransform.position;
+                    diamondGenerator.transform.position = (cameraTransform.forward * (fourthParamaterGemstones * 3f + fifthParamaterGemstones * 2f)) + cameraTransform.position;
                     cameraTransform.LookAt(diamondGenerator.transform);
-                    diamondGenerator.CreateMesh(fourthParamaterCrystals, fifthParamaterCrystals, sixthParamaterCrystals, thirdParamaterCrystals, seventhParameterCrystals, crystalMaterial);
-                    Debug.Log("Crystal-Generate Button was pressed"); // Gibt eine Logmeldung aus   
+                    diamondGenerator.CreateMesh(fourthParamaterGemstones, fifthParamaterGemstones, sixthParamaterGemstones, thirdParamaterGemstones, seventhParameterGemstones, gemstoneMaterial);
+                    Debug.Log("Gemstones-Generate Button was pressed"); // Gibt eine Logmeldung aus   
                     Selection.activeGameObject = diamondGenerator.gameObject;
                     SceneView.lastActiveSceneView.FrameSelected();
                 }
             }
+        }
+
+        // Anzeige für den Helpteil => Infos etc.
+        if (toolbarInt == 2)
+        {
+            GUILayout.Space(5);
+
+            guiStyle.fontSize = 20;
+            GUILayout.Label("Rock Builder - Help", guiStyle);
+
+            GUILayout.Space(15);
+
+            EditorGUILayout.HelpBox("Rock Builder is a simple tool to create rocks and gemstones with different shaders. It's made by two students from the SAE Zurich as their bachelor thesis.", MessageType.Info);
+
+            GUILayout.Space(15);
+
+            if (GUILayout.Button("Open user manual", GUILayout.Height(25)))
+            {
+                // Öffnet das Usermanual PDF-File
+                Application.OpenURL(System.Environment.CurrentDirectory + "/Assets/Rock Builder/Assets/Resources/Dummy.pdf");
+                Debug.Log("User manual Button was pressed"); // Gibt eine Logmeldung aus
+            }
+
+            GUILayout.Space(20);
+
+            GUILayout.Label("Contact", guiStyle);
+
+            GUILayout.Space(15);
+
+            // Mit einem Klick öffnet sich direkt die Mailmaske
+            if (GUILayout.Button("Email: RockBuilder@hotmail.com", EditorStyles.label))
+                Application.OpenURL("mailto:RockBuilder@hotmail.com?");
         }
     }
 
@@ -266,7 +283,7 @@ public class RockBuilderWindow : EditorWindow
         {
             fileData = File.ReadAllBytes(filePath);
             tex = new Texture2D(2, 2);
-            tex.LoadImage(fileData); //..this will auto-resize the texture dimensions.
+            tex.LoadImage(fileData); // This will auto-resize the texture dimensions
         }
         return tex;
     }
