e63504c85177f6de30e7a288f8626edaee7028df
Stefano <c.stefano6@gmail.com>
2020-01-12 22:14:08 +0100

    Added first Unit Tests

---
+++ Assets/Rock Builder/Editor/Tests/Generator/Rocks/StandardRock/Sphere/SphereRockServiceTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+using RockBuilder;
+using UnityEngine;
+
+namespace Tests
+{
+    public class SphereRockServiceTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void CreateEmptySphereRock_Test()
+        {
+            //ARRANGE
+            SphereRock sphereRock = SphereRockService.Instance.CreateEmptySphereRock();
+
+            //ACT
+
+
+            //ASSERT
+            Assert.IsNotNull(sphereRock.gameObject.GetComponent<MeshFilter>());
+            Assert.IsNotNull(sphereRock.gameObject.GetComponent<MeshRenderer>());
+
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator SphereRockMeshGeneratorWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
